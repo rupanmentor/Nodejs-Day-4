c096af11a5a8dcaf941ce9b5177f8414ccb53150
rupanmentor <rupanguvimentor@gmail.com>
2025-11-27 19:28:48 +0530

    Model completed

---
+++ Models/productSchema.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+const products = mongoose.model("products", productSchema);
+
+export default products;
